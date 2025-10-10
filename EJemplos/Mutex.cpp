371fa8c36b3366207f6f82afd61bf276bd3f2330
Fabdovah <85531102+Fabdovah@users.noreply.github.com>
2025-10-10 06:16:27 -0600

    Create Mutex.cpp

---
+++ EJemplos/Mutex.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+using namespace std;
+
+mutex mtx;
+int contador = 0;
+
+void incrementar() {
+    for (int i = 0; i < 1000; i++) {
+        mtx.lock();
+        contador++;
+        mtx.unlock();
+    }
+}
+
+int main() {
+    thread t1(incrementar);
+    thread t2(incrementar);
+    t1.join();
+    t2.join();
+    cout << "Contador final: " << contador << endl;
+}
+/*
+proteger variables compartidas.
+*/

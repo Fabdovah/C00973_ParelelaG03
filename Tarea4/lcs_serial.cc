49a6f58b50fe4267da9969613e21a5df8154a0c0
Fabian <fabian.barquerovarela@ucr.ac.cr>
2025-11-29 07:45:39 -0600

    Final con chrono

--- Tarea4/lcs_serial.cc
+++ Tarea4/lcs_serial.cc
@@ -1,13 +1,45 @@
-//Reconstruir la subsecuencia LCS completa
-
+// Probable final con medición de tiempo
 
 #include <bits/stdc++.h>
+#include <chrono>
 #include "adn.h"
 
 using namespace std;
+using namespace std::chrono;
+
+pair<string,int> LCS(const string &A, const string &B) {
+    int n = A.size();
+    int m = B.size();
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            if (A[i-1] == B[j-1])
+                dp[i][j] = dp[i-1][j-1] + 1;
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+
+    int i = n, j = m;
+    string res;
+    res.reserve(dp[n][m]);
+
+    while (i > 0 && j > 0) {
+        if (A[i-1] == B[j-1]) { 
+            res.push_back(A[i-1]);
+            i--; j--;
+        }
+        else if (dp[i-1][j] >= dp[i][j-1]) i--;
+        else j--;
+    }
+
+    reverse(res.begin(), res.end());
+    return {res, dp[n][m]};
+}
 
 int main(int argc, char** argv) {
-    int len1 = 20, len2 = 20;
+    int len1 = 2000, len2 = 2000;
     if (argc >= 3) {
         len1 = atoi(argv[1]);
         len2 = atoi(argv[2]);
@@ -19,42 +51,17 @@ int main(int argc, char** argv) {
     string S1 = a1->toString();
     string S2 = a2->toString();
 
-    int n = S1.size();
-    int m = S2.size();
+    cout << "Longitudes: S1=" << S1.size() << " S2=" << S2.size() << endl;
 
-    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+    auto t0 = high_resolution_clock::now();
+    auto out = LCS(S1, S2);
+    auto t1 = high_resolution_clock::now();
 
-    // Llenar DP
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= m; ++j) {
-            if (S1[i-1] == S2[j-1])
-                dp[i][j] = dp[i-1][j-1] + 1;
-            else
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-        }
-    }
+    double elapsed = duration<double>(t1 - t0).count();
 
-    // Reconstrucción
-    int i = n, j = m;
-    string lcs;
-
-    while (i > 0 && j > 0) {
-        if (S1[i-1] == S2[j-1]) {
-            lcs.push_back(S1[i-1]);
-            i--; j--;
-        }
-        else if (dp[i-1][j] >= dp[i][j-1]) {
-            i--;
-        }
-        else {
-            j--;
-        }
-    }
-
-    reverse(lcs.begin(), lcs.end());
-
-    cout << "Longitud LCS = " << dp[n][m] << endl;
-    cout << "LCS = " << lcs << endl;
+    cout << "LCS length = " << out.second << endl;
+    cout << "LCS string = " << out.first << endl;
+    cout << "Tiempo (s) = " << elapsed << endl;
 
     delete a1;
     delete a2;

615f5af1dba491bfbc07c0e5a83f1b6889b8ea89
Fabian <fabian.barquerovarela@ucr.ac.cr>
2025-11-29 07:11:59 -0600

    Avance 2

--- Tarea4/lcs_serial.cc
+++ Tarea4/lcs_serial.cc
@@ -1,29 +1,33 @@
-//Inicializar programa, generar secuencias ADN y mostrarlas
-
+//Crear matriz DP e inicializarla
 #include <bits/stdc++.h>
 #include "adn.h"
 
 using namespace std;
 
 int main(int argc, char** argv) {
-    int len1 = 20;
-    int len2 = 20;
-
-    // Si se pasan longitudes por parámetros
+    int len1 = 20, len2 = 20;
     if (argc >= 3) {
         len1 = atoi(argv[1]);
         len2 = atoi(argv[2]);
     }
 
-    // Crear secuencias ADN 
     ADN *a1 = new ADN(len1);
     ADN *a2 = new ADN(len2);
 
     string S1 = a1->toString();
     string S2 = a2->toString();
 
-    cout << "Secuencia 1 (" << S1.size() << "): " << S1 << endl;
-    cout << "Secuencia 2 (" << S2.size() << "): " << S2 << endl;
+    int n = S1.size();
+    int m = S2.size();
+
+    // Matriz DP (n+1) x (m+1)
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+    // Inicialización
+    for (int i = 0; i <= n; ++i) dp[i][0] = 0;
+    for (int j = 0; j <= m; ++j) dp[0][j] = 0;
+
+    cout << "DP inicializada con tamaño (" << n+1 << " x " << m+1 << ")\n";
 
     delete a1;
     delete a2;
